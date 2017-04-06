485c430ac666564e9ebd29b03f31bc2db515ff82
Mochamad Gufron <mgufronefendi@gmail.com>
2017-04-06 17:53:18 +0700

    fix codacy

--- src/KWApi/Services/GoalService.php
+++ src/KWApi/Services/GoalService.php
@@ -7,12 +7,12 @@ class GoalService extends AbstractService
     /**
      * get the goal based on the given id
      *
-     * @param int $id KW UID that will be sent 
+     * @param int $id KW UID that will be sent
      *
      * @return \KWApi\Models\Response Return response object
      */
-    public function getGoal($id)
+    public function getGoal($goal_id)
     {
-        return $this->get("goal/{$id}");
+        return $this->get("goal/{$goal_id}");
     }
 }
