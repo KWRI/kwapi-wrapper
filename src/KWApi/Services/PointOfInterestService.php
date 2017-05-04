5895bf896fba9d010126be1290846bfbcdaa4e42
olegfedoliak <oleg.fedoliak@gmail.com>
2017-05-04 13:41:52 +0300

    Google places service added

---
+++ src/KWApi/Services/PointOfInterestService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace KWApi\Services;
+
+class PointOfInterestService extends AbstractService
+{
+    /**
+     * Return places
+     * @param $address
+     * @param $keyword
+     * @param $type
+     * @return \KWApi\Models\Response
+     */
+    public function getPlaces($address, $keyword, $type)
+    {
+        return $this->get("places/get_places", ['address' => $address, 'keyword' => $keyword, 'type' => $type]);
+    }
+
+    /**
+     * Validate Address
+     * @param $address
+     * @return \KWApi\Models\Response
+     */
+    public function validateAddress($address)
+    {
+        return $this->get("places/validate", ['address' => $address]);
+    }
+}
